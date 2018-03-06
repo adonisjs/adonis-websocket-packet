b87b145f92edd10bc59d89ae25945ce963615850
Harminder <virk.officials@gmail.com>
2018-03-06 12:02:10 +0530

    chore(package): update build process

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,19 @@ module.exports = {
     filename: 'browser.js',
     library: ['adonis', 'wsp'],
     libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: ['node_modules'],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
   }
 }
